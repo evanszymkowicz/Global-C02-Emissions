9816ccd331a0c9c1297966583b95432284affeaf
Evan Szymkowicz <evanszymkowicz@users.noreply.github.com>
2019-01-17 19:30:36 -0500

    Rewrite

--- app.js
+++ app.js
@@ -1,242 +1,101 @@
 d3.queue()
-  .defer(d3.csv, '/data/co2/API_EN.ATM.CO2E.KT_DS2_en_csv_v2.csv', formatter)
-  .defer(d3.csv, '/data/methane/API_EN.ATM.METH.KT.CE_DS2_en_csv_v2.csv', formatter)
-  .defer(d3.csv, '/data/renewables/API_EG.FEC.RNEW.ZS_DS2_en_csv_v2.csv', formatter)
-  .defer(d3.csv, '/data/population/API_SP.POP.TOTL_DS2_en_csv_v2.csv', formatter)
-  .defer(d3.csv, '/data/urban_population/API_SP.URB.TOTL_DS2_en_csv_v2.csv', formatter)
-  .awaitAll(function (error, data) {
+  .defer(d3.json, "//unpkg.com/world-atlas@1.1.4/world/50m.json")
+  .defer(d3.csv, "./data/all_data.csv", function(row) {
+    return {
+      continent: row.Continent,
+      country: row.Country,
+      countryCode: row["Country Code"],
+      emissions: +row["Emissions"],
+      emissionsPerCapita: +row["Emissions Per Capita"],
+      region: row.Region,
+      year: +row.Year
+    }
+  })
+  .await(function(error, mapData, data) {
     if (error) throw error;
 
-var width = 700;
-var height = 700;
-var padding = 100;
-var yearObj = formatAllData(data);
-var yearRange = d3.extent(Object.keys(yearObj).map(year => +year));
+    var extremeYears = d3.extent(data, d => d.year);
+    var currentYear = extremeYears[0];
+    var currentDataType = d3.select('input[name="data-type"]:checked')
+                            .attr("value");
+    var geoData = topojson.feature(mapData, mapData.objects.countries).features;
 
-d3.select('input')
-    .property('min', yearRange[0])
-    .property('max', yearRange[1])
-    .property('value', yearRange[0])
-    .on('input', () => drawPlot(+d3.event.target.value))
-    var svg = d3.select('svg')
-                    .attr('width', width)
-                    .attr('height', height);
+    var width = +d3.select(".chart-container")
+                   .node().offsetWidth;
+    var height = 300;
 
-        svg.append('g')
-            .attr('transform', 'translate(0, ' + (width - padding + 30) + ')')
-            .classed('x-axis', true);
+    createMap(width, width * 4 / 5);
+    createPie(width, height);
+    createBar(width, height);
+    drawMap(geoData, data, currentYear, currentDataType);
+    drawPie(data, currentYear);
+    drawBar(data, currentDataType, "");
 
-        svg.append('g')
-            .attr('transform', 'translate(' + (padding - 30) + ',0)')
-            .classed('y-axis', true);
+    d3.select("#year")
+        .property("min", currentYear)
+        .property("max", extremeYears[1])
+        .property("value", currentYear)
+        .on("input", () => {
+          currentYear = +d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+          drawPie(data, currentYear);
+          highlightBars(currentYear);
+        });
 
-        svg.append('text')
-            .text('CO2 Emissions (kt per person)')
-            .attr('x', width / 2)
-            .attr('y', height)
-            .attr('dy', '-1.5em')
-            .attr('text-anchor', 'middle');
+    d3.selectAll('input[name="data-type"]')
+        .on("change", () => {
+          var active = d3.select(".active").data()[0];
+          var country = active ? active.properties.country : "";
+          currentDataType = d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+          drawBar(data, currentDataType, country);
+        });
 
-        svg.append('text')
-            .text('Methane Emissions (kt of CO2 equivalent per person)')
-            .attr('transform', 'rotate(-90)')
-            .attr('x', - width / 2)
-            .attr('y', '1.5em')
-            .attr('text-anchor', 'middle');
+    d3.selectAll("svg")
+        .on("mousemove touchmove", updateTooltip);
 
-        svg.append('text')
-            .attr('x', width / 2)
-            .attr('y', '2em')
-            .attr('text-anchor', 'middle')
-            .style('font-size', '1.5em')
-            .classed('title', true);
-
-        d3.select('input')
-            .property('min', yearRange[0])
-            .property('max', yearRange[1])
-            .property('value', yearRange[0])
-            .on('input', () => drawPlot(+d3.event.target.value));
-
-drawPlot(yearRange[0])
-
-function drawPlot(year) {
-      var data = yearObj[year];
-      var xScale = d3.scaleLinear()
-                     .domain(d3.extent(data, d => d.co2 / d.population))
-                     .range([padding, width - padding]);
-
-      var yScale = d3.scaleLinear()
-                     .domain(d3.extent(data, d => d.methane / d.population))
-                     .range([height - padding, padding]);
-
-      var fScale = d3.scaleLinear()
-                     .domain([0, 100])
-                     .range(['black', 'green']);
-
-      var rScale = d3.scaleLinear()
-                     .domain([0, 1])
-                     .range([5, 30]);
-
-      d3.select('.x-axis')
-          .call(d3.axisBottom(xScale));
-
-      d3.select('.y-axis')
-          .call(d3.axisLeft(yScale));
-
-      d3.select('.title')
-          .text('Methane vs. CO2 emissions per capita (' + year + ')');
-
-      var update = svg.selectAll('circle')
-                      .data(data, d => d.region);
-
-      update
-        .exit()
-        .transition()
-          .duration(500)
-          .attr('r', 0)
-        .remove();
-
-      update
-        .enter()
-        .append('circle')
-          .on('mousemove touchmove', showTooltip)
-          .on('mouseout touchend', hideTooltip)
-          .attr('cx', d => xScale(d.co2 / d.population))
-          .attr('cy', d => yScale(d.methane / d.population))
-          .attr('stroke', 'white')
-          .attr('stroke-width', 1)
-        .merge(update)
-          .transition()
-          .duration(500)
-          .delay((d, i) => i * 5)
-            .attr('cx', d => xScale(d.co2 / d.population))
-            .attr('cy', d => yScale(d.methane / d.population))
-            .attr('fill', d => fScale(d.renewable))
-            .attr('r', d => rScale(d.urban / d.population));
-    }
-
-    function showTooltip(d) {
-      var tooltip = d3.select('.tooltip');
+    function updateTooltip() {
+      var tooltip = d3.select(".tooltip");
+      var tgt = d3.select(d3.event.target);
+      var isCountry = tgt.classed("country");
+      var isBar = tgt.classed("bar");
+      var isArc = tgt.classed("arc");
+      var dataType = d3.select("input:checked")
+                       .property("value");
+      var units = dataType === "emissions" ? "thousand metric tons" : "metric tons per capita";
+      var data;
+      var percentage = "";
+      if (isCountry) data = tgt.data()[0].properties;
+      if (isArc) {
+        data = tgt.data()[0].data;
+        percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`;
+      }
+      if (isBar) data = tgt.data()[0];
       tooltip
-          .style('opacity', 1)
-          .style('left', ( d3.event.pageX - tooltip.node().offsetWidth / 2 ) + 'px' )
-          .style('top', ( d3.event.pageY - tooltip.node().offsetHeight - 10 ) + 'px')
-          .html(`
-             <p>Region: ${d.region}</p>
-             <p>Methane per capita: ${(d.methane / d.population).toFixed(4)}</p>
-             <p>CO2 per capita: ${(d.co2 / d.population).toFixed(4)}</p>
-             <p>Renewable energy: ${d.renewable.toFixed(2)}%</p>
-             <p>Urban population: ${(d.urban / d.population * 100).toFixed(2)}%</p>
-          `)
-    }
-
-    function hideTooltip(d) {
-      d3.select('.tooltip')
-          .style('opacity', 0);
-    }
-
-var yearObj = formatAllData(data);
-//This will be refacotred to include D3.js nest, but for now many loops because why nat!!!//
-//TODO: What does this do?//
-    function formatAllData(data) {
-      var yearObj = {};
-      data.forEach(function(arr) {
-        var indicator = arr[0].indicator.split(' ')[0].replace(',','').toLowerCase();
-        arr.forEach(function(obj) {
-          // get current region
-          var region = obj.region;
-          // parse through every year, add that region's data to that year array
-          for (var year in obj) {
-            if (parseInt(year)) {
-              if (!yearObj[year]) yearObj[year] = [];
-              var yearArr = yearObj[year];
-              var regionObj = yearArr.find(el => el.region === region);
-              if (regionObj) regionObj[indicator] = obj[year];
-              else {
-                var newObj = {region: region};
-                newObj[indicator] = obj[year];
-                yearArr.push(newObj);
-              }
-            }
-          }
-        })
-        console.log(data);
-      });
-      //Formatter will remove iterations that don't have complete datasets//
-
-      for (var year in yearObj) {
-        yearObj[year] = yearObj[year].filter(validRegion);
-        if (yearObj[year].length === 0) delete yearObj[year];
+          .style("opacity", +(isCountry || isArc || isBar))
+          .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+          .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+      if (data) {
+        var dataValue = data[dataType] ?
+          data[dataType].toLocaleString() + " " + units :
+          "Data Not Available";
+        tooltip
+            .html(`
+              <p>Country: ${data.country}</p>
+              <p>${formatDataType(dataType)}: ${dataValue}</p>
+              <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+              ${percentage}
+            `)
       }
-      return yearObj;
-    }
-
-    function validRegion(d) {
-      for (var key in d) {
-        if (d[key] === null) return false;
-      }
-      return true;
     }
   });
 
-  function formatter(row) {
-    var invalidRows = [
-    'Arab World',
-    'Central Europe and the Baltics',
-    'Caribbean small states',
-    'East Asia & Pacific (excluding high income)',
-    'Early-demographic dividend',
-    'East Asia & Pacific',
-    'Europe & Central Asia (excluding high income)',
-    'Europe & Central Asia',
-    'Euro area',
-    'European Union',
-    'Fragile and conflict affected situations',
-    'High income',
-    'Heavily indebted poor countries (HIPC)',
-    'IBRD only',
-    'IDA & IBRD total',
-    'IDA total',
-    'IDA blend',
-    'IDA only',
-    'Not classified',
-    'Latin America & Caribbean (excluding high income)',
-    'Latin America & Caribbean',
-    'Least developed countries: UN classification',
-    'Low income',
-    'Lower middle income',
-    'Low & middle income',
-    'Late-demographic dividend',
-    'Middle East & North Africa',
-    'Middle income',
-    'Middle East & North Africa (excluding high income)',
-    'North America',
-    'OECD members',
-    'Other small states',
-    'Pre-demographic dividend',
-    'Pacific island small states',
-    'Post-demographic dividend',
-    'Sub-Saharan Africa (excluding high income)',
-    'Sub-Saharan Africa',
-    'Small states',
-    'East Asia & Pacific (IDA & IBRD countries)',
-    'Europe & Central Asia (IDA & IBRD countries)',
-    'Latin America & the Caribbean (IDA & IBRD countries)',
-    'Middle East & North Africa (IDA & IBRD countries)',
-    'South Asia (IDA & IBRD)',
-    'Sub-Saharan Africa (IDA & IBRD countries)',
-    'Upper middle income',
-    'World'
-  ];
-  var obj = {
-    region: row['Country Name'],
-    indicator: row['Indicator Name'],
-  };
-  if (invalidRows.indexOf(obj.region) > -1) return;
-  for (var key in row) {
-    if (parseInt(key)) obj[key] = +row[key] || null;
-  }
+function formatDataType(key) {
+  return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => " " + c);
+}
 
-  return obj;
-
-  }
+function getPercentage(d) {
+  var angle = d.endAngle - d.startAngle;
+  var fraction = 100 * angle / (Math.PI * 2);
+  return fraction.toFixed(2) + "%";
+}
